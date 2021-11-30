fe4a6d3a15466c1c1ac329a4cc5a9903d7c75677
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2021-11-30 21:31:38 +0100

    Refined the 3 controller cases. Changed figures to be induvidual

--- assignment1/code/question5.m
+++ assignment1/code/question5.m
@@ -2,19 +2,47 @@
 %% Question 5 - Controller design
 %  Explore three different scenarios:
 %  Scenario 1 - Unity gain controller
-%  Scenario 2 - ??
-%  Scenario 3 - ??
+%  Scenario 2 - k1 = 1, k2 = 0.1
+%  Scenario 3 - k1 = 1, k2 = 10
 %-------------------------------------------------------------------------
 
 %% Scenario 1 - unity gain controller
+
+%Define controller
 L_y = [1 0;0 1];
 L_r = L_y;
 
-G_cl = minreal((eye(size(G)) + G*L_y) \ (G*L_r));
-S    = (eye(size(G)) + G*L_y) \ eye(size(G));
-T    = minreal((eye(size(G)) + G*L_y) \ (G*L_y));
-S_u  = (eye(size(G)) + L_y*G) \ eye(size(G));
+[G_cl, S, T, S_u] = getStabilityFunc(G, L_y, L_r);
 
+legendArray = [];
+legendArray = [legendArray;
+    strcat("k_1 = ", num2str(L_y(1,1)), ", k_2 = ", num2str(L_y(2,2)))];
+
+
+%Plot singular values
+opt = sigmaoptions;
+opt.Grid = 'on';
+opt.YLimMode = 'manual';
+opt.YLim = {[-40, 10]};
+
+plotCL = figure;
+opt.Title.String = "Singular values of Closed Loop System";
+sigmaplot(G_cl,opt)
+
+plotS = figure;
+opt.Title.String = "Singular values of S";
+sigmaplot(S,opt)
+
+plotT = figure;
+opt.Title.String = "Singular values of T";
+sigmaplot(T,opt)
+
+plotSu = figure;
+opt.Title.String = "Singular values of S_u";
+sigmaplot(S_u,opt)
+
+%-------------------------------------------------
+%Plot poles and zeros
 fig3 = figure;
 subplot(2,2,1)
 opt = pzoptions;
@@ -35,49 +63,83 @@ pzplot(S_u,opt)
 
 saveas(fig3, fullfile(figFolder, 'PoleZeros.png'))
 
-%-------------------------------------------------------------------------
-%
-%-------------------------------------------------------------------------
-
-fig4 = figure;
-subplot(2,2,1)
-opt = sigmaoptions;
-opt.Title.String = "Singular values of Closed Loop System";
-sigmaplot(G_cl,opt)
-
-subplot(2,2,2)
-opt.Title.String = "Singular values of S";
-sigmaplot(S,opt)
-
-subplot(2,2,3)
-opt.Title.String = "Singular values of T";
-sigmaplot(T,opt)
-
-subplot(2,2,4)
-opt.Title.String = "Singular values of S_u";
-sigmaplot(S_u,opt)
-
-saveas(fig4, fullfile(figFolder, 'SingularValues.png'))
-
-
-%figure
-%plot(real(pole(G_cl)), imag(pole(G_cl)),'*')
-
-%legend('G_{cl}', 'S', 'T', 'S_u')
-
 %% Second guess
 
-desiredPoles = [-2 -2/4];
-[Ky, prec] = place(G_ss.a, G_ss.b, desiredPoles)
+%Define controller
+L_y = [1 0;0 0.1];
+L_r = L_y;
 
-G_sys = feedback(G_ss, Ky);
+[G_cl, S, T, S_u] = getStabilityFunc(G, L_y, L_r);
 
-%pzmap(G_sys)
+legendArray = [legendArray;
+    strcat("k_1 = ", num2str(L_y(1,1)), ", k_2 = ", num2str(L_y(2,2)))];
+
+%Plot singular values
+figure(plotCL)
+hold on
+sigmaplot(G_cl)
+
+figure(plotS)
+hold on
+sigmaplot(S)
+
+figure(plotT)
+hold on
+sigmaplot(T)
+
+figure(plotSu)
+hold on
+sigmaplot(S_u)
 
 %% Third guess
 
+%Define controller
+L_y = [1 0;0 10];
+L_r = L_y;
+
+[G_cl, S, T, S_u] = getStabilityFunc(G, L_y, L_r);
+
+legendArray = [legendArray;
+    strcat("k_1 = ", num2str(L_y(1,1)), ", k_2 = ", num2str(L_y(2,2)))];
+
+%Plot singular values
+figure(plotCL)
+hold on
+sigmaplot(G_cl)
+legend(legendArray)
+hold off
+
+figure(plotS)
+hold on
+sigmaplot(S)
+legend(legendArray)
+hold off
+
+figure(plotT)
+hold on
+sigmaplot(T)
+legend(legendArray)
+hold off
+
+figure(plotSu)
+hold on
+sigmaplot(S_u)
+legend(legendArray)
+hold off
+
+%% Save figures
+
+saveas(plotCL, fullfile(figFolder, 'plotCL.png'))
+saveas(plotS, fullfile(figFolder, 'plotS.png'))
+saveas(plotT, fullfile(figFolder, 'plotT.png'))
+saveas(plotSu, fullfile(figFolder, 'plotSu.png'))
 
 
-%% Closed loop, sensitivity, complementary, input sensitivity
+%% Calculate stability functions
+function [G_c, S, T, S_u] = getStabilityFunc(G, K_y, K_r)
 
-
+    G_c = minreal((eye(size(G)) + G*K_y) \ (G*K_r));
+    S   = minreal((eye(size(G)) + G*K_y) \ eye(size(G)));
+    T   = minreal((eye(size(G)) + G*K_y) \ (G*K_y));
+    S_u = minreal((eye(size(G)) + K_y*G) \ eye(size(G)));
+end
