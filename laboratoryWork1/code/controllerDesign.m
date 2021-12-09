8ae3fa0e85a532283a6ff72c75f1d6bb82cfb67d
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2021-12-09 15:32:03 +0100

    Attempts to do pole placement for tension system.

--- laboratoryWork1/code/controllerDesign.m
+++ laboratoryWork1/code/controllerDesign.m
@@ -1,8 +1,25 @@
-%% Design speed controller
+%% Setup and specification
 
 %Design choice
 w_n = 2.5;
 zeta = 1.2;
 
+s = tf([1 0],1);
+
+%% Design speed controller
+
 K_i = w_n^2*0.66/0.87
 K_p = (2*zeta*w_n*0.66 - 1)/0.87
+
+%% Design tension controller
+
+secondOrd = w_n^2/(s^2 + 2*zeta*w_n*s + w_n^2)
+myPoles = pole(secondOrd)
+
+firstOrd = 1/(s - 10*real(min(myPoles)))
+
+thirdOrd = firstOrd*secondOrd
+
+K_d = (thirdOrd.Denominator{:}(2) - 8.05)/554
+K_p = (thirdOrd.Denominator{:}(3) - 263)/554
+K_i = (thirdOrd.Denominator{:}(4))/554
